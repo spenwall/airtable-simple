c2c27977b68f2d1c2641932f344a6144740f9734
Spencer Wallace <spencer.wallace@outlook.com>
2020-09-06 23:27:39 -0600

    Add update and create

--- index.js
+++ index.js
@@ -1,25 +1,54 @@
-const Airtable = require('airtable')
+const Airtable = require('airtable');
 
-class Airtable {
-    constructor (api, base, table) {
-        this.base = new Airtable({ api }).base(base)
-        this.table = table
+class AirtableSimple {
+  constructor(api, base, table) {
+    this.base = new Airtable({ apiKey: api }).base(base);
+    this.table = table;
+  }
+
+  async first(column, id) {
+    const select = `AND(${column} = ${id})`;
+
+    try {
+      let row = await this.base(this.table)
+        .select({
+          view: 'Grid view',
+          filterByFormula: select,
+        })
+        .firstPage();
+
+      if (row.length == 0) {
+        return null;
+      }
+
+      return row[0];
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    first(row, id) {
-        const select = `AND(${row} = ${id})`
+  async update(rowId, column, data) {
+    try {
+        await this.base(this.table).update(rowId, {
+          [column]: data
+        }) 
+    } catch (error) {
+     console.log(error)   
+    }
+  }
 
-        try {
-            let row = await base(this.table)
-              .select({
-                view: "Grid view",
-                filterByFormula: select
-              })
-              .firstPage();
-    
-              return row[0]
-        } catch (error) {
-            console.log(error)
+  async create(row) {
+    this.base(this.table).create(
+      row,
+      function(err, record) {
+        if (err) {
+          console.error(err);
+          return;
         }
-    }
+      }
+    )
+  }
 }
+
+
+module.exports = AirtableSimple;
